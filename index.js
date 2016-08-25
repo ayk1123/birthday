37fb41ca781361b9f15bef52e3068d2788bcf4e5
91juhwang <91juhwang@gmail.com>
2016-08-25 15:17:13 -0400

    image transistion added

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // -----------------------Index Page --------------------
 // source arry, changes every 3 seconds
-var imgArry = ["imgs/1.jpg", "imgs/3.jpg", "imgs/4.jpg", "imgs/5.jpg", "imgs/6.jpg", "imgs/7.jpg"]
+var imgArry = ["imgs/1.jpg", "imgs/2.jpg", "imgs/3.jpg", "imgs/4.jpg", "imgs/5.jpg", "imgs/6.jpg", "imgs/7.jpg"]
 var source = document.getElementById("img_source")
 var index = 1
 
-function img_source() {
-	source.setAttribute("src", imgArry[index])
-	index = index + 1
-	if (index > 5) {
-		index = 0
-	}
+function slideShow() {
+    source.className += "fadeOut";
+    setTimeout(function() {
+        source.src = imgArry[index];
+        source.className = "";
+    },1000);
+    index++;
+    if (index == imgArry.length) { index = 0; }
+    setTimeout(slideShow, 4000);
 }
 
-setInterval(img_source,3000)
+slideShow();
