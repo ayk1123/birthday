e039478392f2f42101412fe5c5b717b4c1519000
91juhwang <91juhwang@gmail.com>
2016-08-25 15:19:48 -0400

    transition fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // source arry, changes every 3 seconds
 var imgArry = ["imgs/1.jpg", "imgs/2.jpg", "imgs/3.jpg", "imgs/4.jpg", "imgs/5.jpg", "imgs/6.jpg", "imgs/7.jpg"]
 var source = document.getElementById("img_source")
-var index = 1
+var index = 0
 
 function slideShow() {
     source.className += "fadeOut";
