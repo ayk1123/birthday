2577008500dbb5cbba715c5d33245352af28289c
91juhwang <91juhwang@gmail.com>
2016-08-25 15:22:41 -0400

    transistion fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function slideShow() {
     setTimeout(function() {
         source.src = imgArry[index];
         source.className = "";
-    },1000);
+    },1300);
     index++;
     if (index == imgArry.length) { index = 0; }
     setTimeout(slideShow, 4000);
